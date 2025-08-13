fe4fefc8abe0e981b4a0b225274b113bf03c3cc4
Deeba-M <deebam0831@gmail.com>
2025-08-13 11:22:39 +0530

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
-import AdminDashboard from './dashboards/admin/AdminDashboard';
+// import AdminDashboard from './dashboards/admin/AdminDashboard';
+// import App from './App';
+import AccountantDashboard from './dashboards/accountant/AccountantDashboard';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <AdminDashboard/>
+    <AccountantDashboard/>
     </BrowserRouter>
   </StrictMode>,
 )
